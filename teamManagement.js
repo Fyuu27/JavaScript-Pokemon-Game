1c3158212fbda1b976bbb48f4bfe080df07e2b4f
Fyuu27 <liamfynnw@gmail.com>
2025-07-09 00:47:04 +0200

    Refactor HTML and CSS for consistency; improve layout and styling across multiple pages

--- teamManagement.js
+++ teamManagement.js
@@ -1,16 +1,16 @@
 function choosePokemon(name) {
-    let team = JSON.parse(localStorage.getItem('team')) || [];
+  let team = JSON.parse(localStorage.getItem('team')) || [];
 
-    if (team.length >= 3) {
-        alert('Du hast schon 3 Pokemon du gieriger Hund');
-        window.location.href = 'Opp.html';
-        return;
-    }
-    if (!team.includes(name)) {
-      team.push(name);
-      localStorage.setItem('team', JSON.stringify(team));
-      alert(name + ' wurde deinem Team hinzugefügt!');
-    } else {
-      alert(name + ' ist schon im Team!');
-    }
+  if (team.length >= 3) {
+    alert('Du hast schon 3 Pokemon du gieriger Hund');
+    window.location.href = 'Opp.html';
+    return;
+  }
+  if (!team.includes(name)) {
+    team.push(name);
+    localStorage.setItem('team', JSON.stringify(team));
+    alert(name + ' wurde deinem Team hinzugefügt!');
+  } else {
+    alert(name + ' ist schon im Team!');
+  }
 }
