543631bc35b26609299edcd17840eefcb88eba6b
Fyuu27 <liamfynnw@gmail.com>
2025-07-09 11:30:46 +0200

    Refactor code structure for improved readability and maintainability

--- teamManagement.js
+++ teamManagement.js
@@ -2,7 +2,7 @@ function choosePokemon(name) {
   let team = JSON.parse(localStorage.getItem('team')) || [];
 
   if (team.length >= 3) {
-    alert('Du hast schon 3 Pokemon du gieriger Hund');
+    alert('Du hast schon 3 Pokemon im Team!');
     window.location.href = 'Opp.html';
     return;
   }
@@ -10,7 +10,15 @@ function choosePokemon(name) {
     team.push(name);
     localStorage.setItem('team', JSON.stringify(team));
     alert(name + ' wurde deinem Team hinzugefügt!');
+    if (team.length === 3) {
+      window.location.href = 'Opp.html';
+      return;
+    }
   } else {
     alert(name + ' ist schon im Team!');
+    if (team.length === 3) {
+      window.location.href = 'Opp.html';
+      return;
+    }
   }
 }
